d8e333a984c59e43462f490a5fc204be1970e4c2
CristianN01 <cristiannechifor71@gmail.com>
2024-07-18 00:44:37 +0200

    dato i nomi alle rotte

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[PageController::class, 'index']);
-Route::get('/movies',[PageController::class, 'movies']);
+Route::get('/',[PageController::class, 'index'])->name('guest.home');
+Route::get('/movies',[PageController::class, 'movies'])->name('guest.movies');
