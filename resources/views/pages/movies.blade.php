fc86399b0c971e3a0ea3e6f0fbe456e6046e0791
CristianN01 <cristiannechifor71@gmail.com>
2024-07-18 01:08:42 +0200

    rappresentato in pagina movies tutte le informazioni dei film sotto forma di lista

--- resources/views/pages/movies.blade.php
+++ resources/views/pages/movies.blade.php
@@ -7,5 +7,19 @@
         Movies
     </h1>
 
-    @dump($movies)
+    {{-- @dump($movies) --}}
+
+    <ul>
+        @foreach ($movies as $movie)
+            <li>
+                <pre>
+                    {{ $movie->title; }} : {{ $movie->original_title }}
+                    Nationality: {{ $movie->nationality; }}
+                    Date: {{ $movie->date; }}
+                    Vote: {{ $movie->vote; }}
+                </pre>
+
+            </li>
+        @endforeach
+    </ul>
 @endsection
